cb725475e8b66c3d10dff1716d7f854b1326faff
Lucas Maia <lucassoaresmaia@hotmail.com>
2025-01-03 17:12:38 -0300

    change path name

--- playlist_rules_generator/app.py
+++ playlist_rules_generator/app.py
@@ -1,4 +1,4 @@
-import pandas as pd 
+import pandas as pd
 import json
 from fpgrowth_py import fpgrowth
 import pickle
@@ -8,47 +8,55 @@ def read_dataset(path):
     playlist_baskets = []
 
     for chunk in pd.read_csv(path, chunksize=50000):
-
-        basket = chunk.groupby('pid')['track_name'].apply(list).tolist()
+        basket = chunk.groupby("pid")["track_name"].apply(list).tolist()
         playlist_baskets.extend(basket)
-    
+
     return playlist_baskets
 
 
 def generate_rules(playlist_baskets, min_support=0.05, min_confidence=0.1):
-    freqItemSet, rules = fpgrowth(playlist_baskets, minSupRatio=min_support, minConf=min_confidence)
-    
-    rules_json = [ { "antecedent": list(rule[0]), "consequent": list(rule[1]), "confidence": rule[2] } for rule in rules ]
+    freqItemSet, rules = fpgrowth(
+        playlist_baskets, minSupRatio=min_support, minConf=min_confidence
+    )
+
+    rules_json = [
+        {
+            "antecedent": list(rule[0]),
+            "consequent": list(rule[1]),
+            "confidence": rule[2],
+        }
+        for rule in rules
+    ]
 
     return rules_json
 
+
 def save_rules(rules, path):
-    with open(path, 'wb') as f:
+    with open(path, "wb") as f:
         pickle.dump(rules, f)
 
+
 def load_rules(path):
-    with open(path, 'rb') as f:
+    with open(path, "rb") as f:
         rules = pickle.load(f)
-    
+
     return rules
 
 
 def main():
-    filepath = '2023_spotify_ds1.csv'
+    filepath = "../mnt/shared/2023_spotify_ds1.csv"
 
-    print('Processing dataset...')
+    print("Processing dataset...")
     playlist_baskests = read_dataset(filepath)
 
-    print('Generating rules...')
+    print("Generating rules...")
     rules = generate_rules(playlist_baskests)
 
-    print('Saving rules...')
-    save_rules(rules, 'rules.pkl')
+    print("Saving rules...")
+    save_rules(rules, "../mnt/shared/rules.pkl")
 
-    print('DONE!')
+    print("DONE!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
-    
